0058cd693ffe366016b921592d8dcef5bceb7500
safach <safa1chaabane@gmail.com>
2023-01-05 15:24:35 +0100

    Add modification

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -31,12 +31,12 @@ function Footer() {
       <Col md={8}>
       <h1  className='footer-title'> CONTACTEZ-NOUS</h1>
       <Row>
-        <Col md={4}><Row><Col md={1} ><img alt="adresse" src={adresse}/></Col><Col  ><p><h5>LUXURY HOTEL</h5><p>Rue de la republique,19</p><p> 1082 Tunis</p><p> Tunisia</p></p></Col></Row>
-        <Row className="contact-informations"><Col md={1} > <img alt="adresse" src={position}/></Col><Col><p> <a className='block-information' href="/">Plan d'accés</a></p></Col></Row></Col>
+        <Col md={4}><Row><Col xs={1} ><img alt="adresse" src={adresse}/></Col><Col  ><p><h5>LUXURY HOTEL</h5><p>Rue de la republique,19</p><p> 1082 Tunis</p><p> Tunisia</p></p></Col></Row>
+        <Row className="contact-informations"><Col xs={1} > <img alt="adresse" src={position}/></Col><Col><p> <a className='block-information' href="/">Plan d'accés</a></p></Col></Row></Col>
         <Col md={4} className="contact-information">
-        <Row ><Col md={1}> <img alt="phone" src={phone}/></Col><Col> <p className='block-information'>Tél.: +21671000000</p></Col></Row>
-        <Row ><Col md={1}><img alt="email" src={email}/></Col><Col><p className='block-information'><a href="mailto:safa1chaabane@gmail.com">contact@luxuryhotel.com</a></p></Col></Row>
-        <Row ><Col md={1}><img alt="work" src={work}/></Col><Col><p className='block-information'>MA 800 50</p></Col></Row>
+        <Row ><Col xs={1}> <img alt="phone" src={phone}/></Col><Col> <p className='block-information'>Tél.: +21671000000</p></Col></Row>
+        <Row ><Col xs={1}><img alt="email" src={email}/></Col><Col><p className='block-information'><a href="mailto:safa1chaabane@gmail.com">contact@luxuryhotel.com</a></p></Col></Row>
+        <Row ><Col xs={1}><img alt="work" src={work}/></Col><Col><p className='block-information'>MA 800 50</p></Col></Row>
         </Col>
         <Col md={4} className="contact-information2">
          <FontAwesomeIcon icon={faFacebookSquare} size="3x"/>
